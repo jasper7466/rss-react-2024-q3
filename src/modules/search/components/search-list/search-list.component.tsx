cd7d0b4b9b1a7e392315364eea2cabf98b1253a8
Tim <jasper7466@yandex.ru>
2024-07-07 03:28:48 +0500

    feat: handle "nothing found" case

--- src/modules/search/components/search-list/search-list.component.tsx
+++ src/modules/search/components/search-list/search-list.component.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import { SearchListItem } from '../search-list-item/search-list-item.component';
 import { IItem } from '../../interfaces/item.interface';
 import './search-list.component.css';
@@ -9,10 +9,18 @@ export class SearchList extends Component<Props, never> {
   render() {
     return (
       <div className="search-list">
-        {this.props.query && <p>Results for: "{this.props.query}"</p>}
-        {this.props.items.map(item => (
-          <SearchListItem key={item.id} {...item} />
-        ))}
+        {this.props.query ? (
+          <Fragment>
+            <p>Results for: "{this.props.query}"</p>
+            {this.props.items.length ? (
+              this.props.items.map(item => <SearchListItem key={item.id} {...item} />)
+            ) : (
+              <p>Nothing found</p>
+            )}
+          </Fragment>
+        ) : (
+          <p>Try searching for something: “phone”, “laptop”, etc...</p>
+        )}
       </div>
     );
   }
