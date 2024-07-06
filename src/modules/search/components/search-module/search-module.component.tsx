3269afbb30a063e85c0cb2fecaf656d0b41e72b6
Tim <jasper7466@yandex.ru>
2024-07-07 03:52:05 +0500

    feat: show "overlay-loader" while fetching data

--- src/modules/search/components/search-module/search-module.component.tsx
+++ src/modules/search/components/search-module/search-module.component.tsx
@@ -3,6 +3,7 @@ import { SearchBar } from '../search-bar/search-bar.component';
 import { SearchList } from '../search-list/search-list.component';
 import { searchService } from '../../services/search.service';
 import { IItem } from '../../interfaces/item.interface';
+import { OverlayLoader } from '../../../../components/overlay-loader/overlay-loader.component';
 import './search-module.component.css';
 
 type Props = Record<string, never>;
@@ -10,23 +11,32 @@ type Props = Record<string, never>;
 type State = {
   items: IItem[];
   query?: string;
+  isLoading: boolean;
 };
 
 export class SearchModule extends Component<Props, State> {
   state: State = {
     items: [],
+    isLoading: false,
   };
 
   async handleSubmit(query: string) {
+    this.setState({ isLoading: true });
+
     const items = await searchService.searchItems(query);
-    this.setState({ items, query });
+
+    this.setState({ items, query, isLoading: false });
   }
 
   render() {
     return (
       <section className="search-module">
         <SearchBar submitHandler={this.handleSubmit.bind(this)} />
-        <SearchList items={this.state.items} query={this.state.query} />
+        {this.state.isLoading ? (
+          <OverlayLoader />
+        ) : (
+          <SearchList items={this.state.items} query={this.state.query} />
+        )}
       </section>
     );
   }
