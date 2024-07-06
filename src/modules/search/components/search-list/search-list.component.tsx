e5b0a5094686708177bffaac80c2c80c60c10536
Tim <jasper7466@yandex.ru>
2024-07-07 02:56:16 +0500

    feat: implement "search-list" component

--- src/modules/search/components/search-list/search-list.component.tsx
+++ src/modules/search/components/search-list/search-list.component.tsx
@@ -1,9 +1,19 @@
 import { Component } from 'react';
-import { SearchBar } from '../search-bar/search-bar.component';
+import { SearchListItem } from '../search-list-item/search-list-item.component';
+import { IItem } from '../../interfaces/item.interface';
 import './search-list.component.css';
 
-export class SearchList extends Component {
+type Props = { items: IItem[]; query?: string };
+
+export class SearchList extends Component<Props, never> {
   render() {
-    return <SearchBar />;
+    return (
+      <div className="search-list">
+        {this.props.query && <p>Results for: "{this.props.query}"</p>}
+        {this.props.items.map(item => (
+          <SearchListItem key={item.id} {...item} />
+        ))}
+      </div>
+    );
   }
 }
