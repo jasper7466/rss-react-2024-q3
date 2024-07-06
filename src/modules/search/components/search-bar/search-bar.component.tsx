d84f478db3ee30db09f081e63038b76417e7ee52
Tim <jasper7466@yandex.ru>
2024-07-07 02:58:02 +0500

    feat: implement "search-bar" component

--- src/modules/search/components/search-bar/search-bar.component.tsx
+++ src/modules/search/components/search-bar/search-bar.component.tsx
@@ -1,8 +1,44 @@
-import { Component } from 'react';
+import { ChangeEvent, Component, KeyboardEvent } from 'react';
 import './search-bar.component.css';
 
-export class SearchBar extends Component {
+type Props = {
+  submitHandler: (query: string) => void;
+};
+
+type State = {
+  query: string;
+};
+export class SearchBar extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { query: '' };
+  }
+
+  handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({ query: event.target.value });
+  }
+
+  handleKeyPress(event: KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter') {
+      this.handleSubmit();
+    }
+  }
+
+  handleSubmit() {
+    this.props.submitHandler(this.state.query);
+  }
+
   render() {
-    return <input placeholder="Enter search query..."></input>;
+    return (
+      <div className="search-bar">
+        <input
+          placeholder="Enter search query..."
+          onChange={this.handleInputChange.bind(this)}
+          onKeyDown={this.handleKeyPress.bind(this)}></input>
+        <button onClick={this.handleSubmit.bind(this)} disabled={this.state.query.length === 0}>
+          Search
+        </button>
+      </div>
+    );
   }
 }
