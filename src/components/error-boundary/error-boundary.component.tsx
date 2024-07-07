2f887ec24619f2bc23b44bb98d55ebedd6eef2b4
Tim <jasper7466@yandex.ru>
2024-07-07 05:38:49 +0500

    feat: implement "error-boundary" component

---
+++ src/components/error-boundary/error-boundary.component.tsx
@@ -0,0 +1,41 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+import './error-boundary.component.css';
+
+type Props = {
+  children: ReactNode;
+};
+
+type State = {
+  hasError: boolean;
+};
+
+export class ErrorBoundary extends Component<Props, State> {
+  state: State = {
+    hasError: false,
+  };
+
+  static getDerivedStateFromError(): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log('Uncaught error:', error, errorInfo);
+  }
+
+  resetErrorBoundary() {
+    this.setState({ hasError: false });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="error-boundary">
+          <h1>Something went wrong :(</h1>
+          <button onClick={this.resetErrorBoundary.bind(this)}>Reset</button>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
