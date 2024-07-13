dd57012953b80b57526be60d969542ecb0682465
Tim <jasper7466@yandex.ru>
2024-07-14 02:51:36 +0500

    feat: integrate "use-local-storage" custom hook

--- src/modules/search/components/search-module/search-module.component.tsx
+++ src/modules/search/components/search-module/search-module.component.tsx
@@ -1,34 +1,39 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import { SearchBar } from '../search-bar/search-bar.component';
 import { SearchList } from '../search-list/search-list.component';
 import { searchService } from '../../services/search.service';
 import { IItem } from '../../interfaces/item.interface';
 import { OverlayLoader } from '../../../../components/overlay-loader/overlay-loader.component';
 import { ErrorThrower } from '../../../../components/error-thrower/error-thrower.component';
+import { useLocalStorage } from '../../../../hooks/use-local-storage.hook';
+import { LOCAL_STORAGE_KEY } from '../../../../config/constants';
 import './search-module.component.css';
 
-type Props = Record<string, never>;
+type LocalData = {
+  query: string;
+};
 
-export const SearchModule: FC<Props> = () => {
+export const SearchModule: FC = () => {
   const [itemsState, setItemsState] = useState<IItem[]>([]);
-  const [queryState, setQueryState] = useState<string | null>(null);
   const [isLoadingState, setIsLoadingState] = useState<boolean>(false);
+  const [data, updateData] = useLocalStorage<LocalData>(LOCAL_STORAGE_KEY);
 
-  const handleSubmit = async (query: string) => {
-    setIsLoadingState(true);
+  const handleSubmit = useCallback(
+    async (query: string) => {
+      setIsLoadingState(true);
 
-    const items = await searchService.searchItems(query);
+      const items = await searchService.searchItems(query);
 
-    setItemsState(items);
-    setQueryState(query);
-    setIsLoadingState(false);
-  };
+      setItemsState(items);
+      updateData({ query });
+      setIsLoadingState(false);
+    },
+    [updateData],
+  );
 
   useEffect(() => {
-    const query = searchService.getLastQuery();
-    setQueryState(query);
-    handleSubmit(query || '');
-  }, []);
+    handleSubmit(data?.query || '');
+  }, [handleSubmit, data?.query]);
 
   return (
     <div className="search-module">
@@ -37,7 +42,11 @@ export const SearchModule: FC<Props> = () => {
       </section>
       <section className="section">
         <ErrorThrower />
-        {isLoadingState ? <OverlayLoader /> : <SearchList items={itemsState} query={queryState} />}
+        {isLoadingState ? (
+          <OverlayLoader />
+        ) : (
+          <SearchList items={itemsState} query={data?.query || ''} />
+        )}
       </section>
     </div>
   );
--- src/modules/search/services/search.service.ts
+++ src/modules/search/services/search.service.ts
@@ -1,31 +1,8 @@
-import { LOCAL_STORAGE_KEY } from '../../../config/constants';
 import { searchItemsEndpoint } from '../api/search.endpoint';
 import { IItem } from '../interfaces/item.interface';
-
-interface ILocalData {
-  lastQuery: string;
-}
 class SearchService {
   public async searchItems(query: string): Promise<IItem[]> {
-    this.setLastQuery(query);
     return await searchItemsEndpoint({ query });
-  }
-
-  public getLastQuery(): string | null {
-    const storage = localStorage.getItem(LOCAL_STORAGE_KEY);
-    const query = storage && (<ILocalData>JSON.parse(storage)).lastQuery;
-
-    return query;
-  }
-
-  private setLastQuery(query: string): void {
-    const storage = localStorage.getItem(LOCAL_STORAGE_KEY);
-    const previousState = storage !== null ? <ILocalData>JSON.parse(storage) : {};
-
-    localStorage.setItem(
-      LOCAL_STORAGE_KEY,
-      JSON.stringify(<ILocalData>{ ...previousState, lastQuery: query || null }),
-    );
   }
 }
 
