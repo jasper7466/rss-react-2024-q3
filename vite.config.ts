6b848f65e7cd678b920a345f64553c4fe8cb825f
Tim <jasper7466@yandex.ru>
2024-07-14 23:56:33 +0500

    chore: configure vitest and react-testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import 'vitest/config'; // <-- just dummy import
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/rss-react-2024-q3-deployments/hooks-and-routing/',
+  test: { environment: 'jsdom', setupFiles: ['./tests/setup.ts'], globals: true },
 });
